ae9f060e77592d4b1618be0856936f2887ecd1cc
LZD <49672475@qq.com>
2022-05-11 20:17:30 +0800

    third commit

--- src/main/java/com/li/git/GItTest.java
+++ src/main/java/com/li/git/GItTest.java
@@ -8,12 +8,6 @@ public class GItTest {
         System.out.println("hello git!");
         System.out.println("hello git2!2");
         System.out.println("hello git3!");
-        System.out.println("hello git4!");
-        System.out.println("master test!");
-        System.out.println("hot-fix test!");
-        System.out.println("push test!");
-        System.out.println("pull test!");
-        System.out.println("gitee test!");
-        System.out.println("gitee test2!");
+
     }
 }
