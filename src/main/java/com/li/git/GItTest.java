50834a95be0d2c4d78fb872d2b83256d03725d04
LZD <49672475@qq.com>
2022-05-11 20:16:18 +0800

    second commit

--- src/main/java/com/li/git/GItTest.java
+++ src/main/java/com/li/git/GItTest.java
@@ -6,7 +6,7 @@ package com.li.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git2!");
+        System.out.println("hello git2!2");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("master test!");
